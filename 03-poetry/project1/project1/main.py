cc22cc8dec3f4c88e8618f1b2b7ac19f6cd1c980
Sir Qasim <m.qasim077@gmail.com>
2024-08-09 19:19:00 +0500

    class3

---
+++ 03-poetry/project1/project1/main.py
@@ -0,0 +1,6 @@
+def add_two_numbers(num1: int, num2: int) -> int:
+    return num1 + num2
+
+print(add_two_numbers(1, 2))
+
+print("Hello, world!")
---
+++ 03-poetry/project1/tests/test_main.py
@@ -0,0 +1,7 @@
+from project1.main import *
+
+def test_add_two_numbers():
+    assert add_two_numbers(1,2) == 3
+
+def test_add_two_numbers1():
+    assert add_two_numbers(4,7) == 6
